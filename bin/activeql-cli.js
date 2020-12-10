8ec87753cca8eacd110a3458684b3f0456005263
Thomas Rekittke <betterobjects@mac.com>
2020-12-10 16:22:13 +0100

    init

--- bin/activeql-cli.js
+++ bin/activeql-cli.js
@@ -12,7 +12,6 @@ var util = require('util')
 
 var MODE_0666 = parseInt('0666', 8)
 var MODE_0755 = parseInt('0755', 8)
-var TEMPLATE_DIR = path.join(__dirname, '..', 'templates')
 var VERSION = require('../package').version
 
 var _exit = process.exit
@@ -101,7 +100,7 @@ function confirm (msg, callback) {
  */
 
 function copyTemplate (from, to) {
-  write(to, fs.readFileSync(path.join(TEMPLATE_DIR, from), 'utf-8'))
+  write(to, fs.readFileSync( from, 'utf-8'))
 }
 
 /**
@@ -109,7 +108,7 @@ function copyTemplate (from, to) {
  */
 
 function copyTemplateMulti (fromDir, toDir, nameGlob) {
-  fs.readdirSync(path.join(TEMPLATE_DIR, fromDir))
+  fs.readdirSync( fromDir )
     .filter(minimatch.filter(nameGlob, { matchBase: true }))
     .forEach(function (name) {
       copyTemplate(path.join(fromDir, name), path.join(toDir, name))
@@ -124,228 +123,88 @@ function copyTemplateMulti (fromDir, toDir, nameGlob) {
  */
 
 function createApplication (name, dir) {
-  console.log()
+  console.log('Build express')
+  createExpressApplication( name, dir );
+  console.log('Done')
+}
 
+function createExpressApplication (name, dir) {
   // Package
   var pkg = {
     name: name,
     version: '0.0.0',
     private: true,
-    scripts: {
-      start: 'node ./bin/www'
+    "scripts": {
+      "clean": "rimraf dist",
+      "build": "rimraf dist && tsc",
+      "watch:build": "tsc --watch",
+      "server": "./node_modules/.bin/ts-node-dev -P ./tsconfig.json --watch ./ --no-notify --inspect -- ./app.ts",
+      "seed": "./node_modules/.bin/ts-node -P ./tsconfig.json -- ./seed.ts",
+      "test": "jest --watch --detectOpenHandles"
     },
-    dependencies: {
-      'debug': '~2.6.9',
-      'express': '~4.16.1'
+      "dependencies": {
+      "activeql-server": "^1.0.0",
+      "apollo-server-express": "^2.9.3",
+      "bcryptjs": "^2.4.3",
+      "compression": "^1.7.4",
+      "dotenv": "^8.2.0",
+      "es6-template-strings": "^2.0.1",
+      "express-jwt": "^6.0.0",
+      "jsonwebtoken": "^8.5.1",
+      "lodash": "^4.17.15",
+      "yaml": "^1.7.2"
+    },
+    "devDependencies": {
+      "@types/bcryptjs": "^2.4.2",
+      "@types/compression": "^1.7.0",
+      "@types/express-jwt": "0.0.42",
+      "@types/jest": "^25.2.3",
+      "@types/jsonwebtoken": "^8.5.0",
+      "@types/lodash": "^4.14.138",
+      "@types/node": "^14.0.9",
+      "jest": "^26.0.1",
+      "nodemon": "^2.0.4",
+      "rimraf": "^3.0.2",
+      "ts-jest": "^26.1.0",
+      "ts-node": "^8.10.2",
+      "ts-node-dev": "^1.0.0-pre.43",
+      "typescript": "^3.9.3"
     }
   }
 
-  // JavaScript
-  var app = loadTemplate('js/app.js')
-  var www = loadTemplate('js/www')
+  dir = path.join( dir, 'express');
+  mkdir(dir, '.')
 
-  // App name
-  www.locals.name = name
+  mkdir(dir, 'domain-configuration')
+  mkdir(dir, 'impl')
+  mkdir(dir, 'uploads')
+  
 
-  // App modules
-  app.locals.localModules = Object.create(null)
-  app.locals.modules = Object.create(null)
-  app.locals.mounts = []
-  app.locals.uses = []
+  var templateDir = path.join(__dirname, '..', 'express-ts-template')
 
-  // Request logger
-  app.locals.modules.logger = 'morgan'
-  app.locals.uses.push("logger('dev')")
-  pkg.dependencies.morgan = '~1.9.1'
+  copyTemplateMulti( templateDir, dir, '*.ts' );
+  copyTemplateMulti( templateDir, dir, '*.json' );
+  copyTemplateMulti( templateDir, dir, 'README.md' );
+  copyTemplateMulti( templateDir + '/impl', dir + '/impl', '*.ts' );
+  copyTemplateMulti( templateDir + '/domain-configuration', dir + '/domain-configuration', '*.yml' );
 
-  // Body parsers
-  app.locals.uses.push('express.json()')
-  app.locals.uses.push('express.urlencoded({ extended: false })')
-
-  // Cookie parser
-  app.locals.modules.cookieParser = 'cookie-parser'
-  app.locals.uses.push('cookieParser()')
-  pkg.dependencies['cookie-parser'] = '~1.4.4'
-
-  if (dir !== '.') {
-    mkdir(dir, '.')
-  }
-
-  mkdir(dir, 'public')
-  mkdir(dir, 'public/javascripts')
-  mkdir(dir, 'public/images')
-  mkdir(dir, 'public/stylesheets')
-
-  // copy css templates
-  switch (program.css) {
-    case 'less':
-      copyTemplateMulti('css', dir + '/public/stylesheets', '*.less')
-      break
-    case 'stylus':
-      copyTemplateMulti('css', dir + '/public/stylesheets', '*.styl')
-      break
-    case 'compass':
-      copyTemplateMulti('css', dir + '/public/stylesheets', '*.scss')
-      break
-    case 'sass':
-      copyTemplateMulti('css', dir + '/public/stylesheets', '*.sass')
-      break
-    default:
-      copyTemplateMulti('css', dir + '/public/stylesheets', '*.css')
-      break
-  }
-
-  // copy route templates
-  mkdir(dir, 'routes')
-  copyTemplateMulti('js/routes', dir + '/routes', '*.js')
-
-  if (program.view) {
-    // Copy view templates
-    mkdir(dir, 'views')
-    pkg.dependencies['http-errors'] = '~1.6.3'
-    switch (program.view) {
-      case 'dust':
-        copyTemplateMulti('views', dir + '/views', '*.dust')
-        break
-      case 'ejs':
-        copyTemplateMulti('views', dir + '/views', '*.ejs')
-        break
-      case 'hbs':
-        copyTemplateMulti('views', dir + '/views', '*.hbs')
-        break
-      case 'hjs':
-        copyTemplateMulti('views', dir + '/views', '*.hjs')
-        break
-      case 'jade':
-        copyTemplateMulti('views', dir + '/views', '*.jade')
-        break
-      case 'pug':
-        copyTemplateMulti('views', dir + '/views', '*.pug')
-        break
-      case 'twig':
-        copyTemplateMulti('views', dir + '/views', '*.twig')
-        break
-      case 'vash':
-        copyTemplateMulti('views', dir + '/views', '*.vash')
-        break
-    }
-  } else {
-    // Copy extra public files
-    copyTemplate('js/index.html', path.join(dir, 'public/index.html'))
-  }
-
-  // CSS Engine support
-  switch (program.css) {
-    case 'compass':
-      app.locals.modules.compass = 'node-compass'
-      app.locals.uses.push("compass({ mode: 'expanded' })")
-      pkg.dependencies['node-compass'] = '0.2.3'
-      break
-    case 'less':
-      app.locals.modules.lessMiddleware = 'less-middleware'
-      app.locals.uses.push("lessMiddleware(path.join(__dirname, 'public'))")
-      pkg.dependencies['less-middleware'] = '~2.2.1'
-      break
-    case 'sass':
-      app.locals.modules.sassMiddleware = 'node-sass-middleware'
-      app.locals.uses.push("sassMiddleware({\n  src: path.join(__dirname, 'public'),\n  dest: path.join(__dirname, 'public'),\n  indentedSyntax: true, // true = .sass and false = .scss\n  sourceMap: true\n})")
-      pkg.dependencies['node-sass-middleware'] = '0.11.0'
-      break
-    case 'stylus':
-      app.locals.modules.stylus = 'stylus'
-      app.locals.uses.push("stylus.middleware(path.join(__dirname, 'public'))")
-      pkg.dependencies['stylus'] = '0.54.5'
-      break
-  }
-
-  // Index router mount
-  app.locals.localModules.indexRouter = './routes/index'
-  app.locals.mounts.push({ path: '/', code: 'indexRouter' })
-
-  // User router mount
-  app.locals.localModules.usersRouter = './routes/users'
-  app.locals.mounts.push({ path: '/users', code: 'usersRouter' })
-
-  // Template support
-  switch (program.view) {
-    case 'dust':
-      app.locals.modules.adaro = 'adaro'
-      app.locals.view = {
-        engine: 'dust',
-        render: 'adaro.dust()'
-      }
-      pkg.dependencies.adaro = '~1.0.4'
-      break
-    case 'ejs':
-      app.locals.view = { engine: 'ejs' }
-      pkg.dependencies.ejs = '~2.6.1'
-      break
-    case 'hbs':
-      app.locals.view = { engine: 'hbs' }
-      pkg.dependencies.hbs = '~4.0.4'
-      break
-    case 'hjs':
-      app.locals.view = { engine: 'hjs' }
-      pkg.dependencies.hjs = '~0.0.6'
-      break
-    case 'jade':
-      app.locals.view = { engine: 'jade' }
-      pkg.dependencies.jade = '~1.11.0'
-      break
-    case 'pug':
-      app.locals.view = { engine: 'pug' }
-      pkg.dependencies.pug = '2.0.0-beta11'
-      break
-    case 'twig':
-      app.locals.view = { engine: 'twig' }
-      pkg.dependencies.twig = '~0.10.3'
-      break
-    case 'vash':
-      app.locals.view = { engine: 'vash' }
-      pkg.dependencies.vash = '~0.12.6'
-      break
-    default:
-      app.locals.view = false
-      break
-  }
-
-  // Static files
-  app.locals.uses.push("express.static(path.join(__dirname, 'public'))")
-
-  if (program.git) {
-    copyTemplate('js/gitignore', path.join(dir, '.gitignore'))
-  }
-
-  // sort dependencies like npm(1)
-  pkg.dependencies = sortedObject(pkg.dependencies)
-
-  // write files
-  write(path.join(dir, 'app.js'), app.render())
   write(path.join(dir, 'package.json'), JSON.stringify(pkg, null, 2) + '\n')
-  mkdir(dir, 'bin')
-  write(path.join(dir, 'bin/www'), www.render(), MODE_0755)
 
   var prompt = launchedFromCmd() ? '>' : '$'
 
-  if (dir !== '.') {
-    console.log()
-    console.log('   change directory:')
-    console.log('     %s cd %s', prompt, dir)
-  }
+  console.log()
+  console.log('   change directory:')
+  console.log('     %s cd %s', prompt, dir)
 
   console.log()
   console.log('   install dependencies:')
-  console.log('     %s npm install', prompt)
-  console.log()
-  console.log('   run the app:')
-
-  if (launchedFromCmd()) {
-    console.log('     %s SET DEBUG=%s:* & npm start', prompt, name)
-  } else {
-    console.log('     %s DEBUG=%s:* npm start', prompt, name)
-  }
+  console.log('     %s npm install', prompt)  
 
   console.log()
+  console.log('   run server:')
+  console.log('     %s npm run server', prompt)
+  console.log()
+
 }
 
 /**
@@ -440,21 +299,6 @@ function main () {
 
   // App name
   var appName = createAppName(path.resolve(destinationPath)) || 'hello-world'
-
-  // View engine
-  if (program.view === true) {
-    if (program.ejs) program.view = 'ejs'
-    if (program.hbs) program.view = 'hbs'
-    if (program.hogan) program.view = 'hjs'
-    if (program.pug) program.view = 'pug'
-  }
-
-  // Default view engine
-  if (program.view === true) {
-    warning('the default view engine will not be jade in future releases\n' +
-      "use `--view=jade' or `--help' for additional options")
-    program.view = 'jade'
-  }
 
   // Generate application
   emptyDirectory(destinationPath, function (empty) {
---
+++ express-ts-template/README.md
@@ -0,0 +1,25 @@
+This is the starter app for 
+
+# ActiveQL
+
+
+### Run the GraphQL API Express Server
+
+```
+cd express
+npm run server
+```
+
+### Run the Admin UI 
+
+```
+cd angular
+ng serve
+```
+
+### Run the GraphQL API Express Server and Admin UI
+
+```
+cd express
+npm run start
+```
