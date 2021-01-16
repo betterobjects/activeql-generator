ef91bca25770d49d130baea332520205ca67a361
Thomas Rekittke <betterobjects@mac.com>
2021-01-16 13:30:39 +0100

    bump version

--- bin/activeql-cli.js
+++ bin/activeql-cli.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 
 var copy = require('recursive-copy');
-var ejs = require('ejs')
 var fs = require('fs')
-var minimatch = require('minimatch')
-var mkdirp = require('mkdirp')
 var path = require('path')
 var program = require('commander')
 var readline = require('readline')
-var sortedObject = require('sorted-object')
-var util = require('util')
 
-var MODE_0666 = parseInt('0666', 8)
-var MODE_0755 = parseInt('0755', 8)
 var VERSION = require('../package').version
 
 var _exit = process.exit
@@ -96,25 +89,6 @@ function confirm (msg, callback) {
   })
 }
 
-/**
- * Copy file from template directory.
- */
-
-function copyTemplate (from, to) {
-  write(to, fs.readFileSync( from, 'utf-8'))
-}
-
-/**
- * Copy multiple files from template directory.
- */
-
-function copyTemplateMulti (fromDir, toDir, nameGlob) {
-  fs.readdirSync( fromDir )
-    .filter(minimatch.filter(nameGlob, { matchBase: true }))
-    .forEach(function (name) {
-      copyTemplate(path.join(fromDir, name), path.join(toDir, name))
-    })
-}
 
 /**
  * Create application at the given directory.
@@ -134,84 +108,6 @@ function createApplication (name, dir) {
   });
 }
 
-function createExpressApplication (name, dir) {
-  // Package
-  var pkg = {
-    name: name,
-    version: '0.0.0',
-    private: true,
-    "scripts": {
-      "clean": "rimraf dist",
-      "build": "rimraf dist && tsc",
-      "watch:build": "tsc --watch",
-      "server": "./node_modules/.bin/ts-node-dev -P ./tsconfig.json --watch ./ --no-notify --inspect -- ./app.ts",
-      "seed": "./node_modules/.bin/ts-node -P ./tsconfig.json -- ./seed.ts",
-      "test": "jest --watch --detectOpenHandles"
-    },
-      "dependencies": {
-      "activeql-server": "^1.0.0",
-      "apollo-server-express": "^2.9.3",
-      "bcryptjs": "^2.4.3",
-      "compression": "^1.7.4",
-      "dotenv": "^8.2.0",
-      "es6-template-strings": "^2.0.1",
-      "express-jwt": "^6.0.0",
-      "jsonwebtoken": "^8.5.1",
-      "lodash": "^4.17.15",
-      "yaml": "^1.7.2"
-    },
-    "devDependencies": {
-      "@types/bcryptjs": "^2.4.2",
-      "@types/compression": "^1.7.0",
-      "@types/express-jwt": "0.0.42",
-      "@types/jest": "^25.2.3",
-      "@types/jsonwebtoken": "^8.5.0",
-      "@types/lodash": "^4.14.138",
-      "@types/node": "^14.0.9",
-      "jest": "^26.0.1",
-      "nodemon": "^2.0.4",
-      "rimraf": "^3.0.2",
-      "ts-jest": "^26.1.0",
-      "ts-node": "^8.10.2",
-      "ts-node-dev": "^1.0.0-pre.43",
-      "typescript": "^3.9.3"
-    }
-  }
-
-  dir = path.join( dir, 'express');
-  mkdir(dir, '.')
-
-  mkdir(dir, 'domain-configuration')
-  mkdir(dir, 'impl')
-  mkdir(dir, 'uploads')
-  
-
-  var templateDir = path.join(__dirname, '..', 'express-ts-template')
-
-  copyTemplateMulti( templateDir, dir, '*.ts' );
-  copyTemplateMulti( templateDir, dir, '*.json' );
-  copyTemplateMulti( templateDir, dir, 'README.md' );
-  copyTemplateMulti( templateDir + '/impl', dir + '/impl', '*.ts' );
-  copyTemplateMulti( templateDir + '/domain-configuration', dir + '/domain-configuration', '*.yml' );
-
-  write(path.join(dir, 'package.json'), JSON.stringify(pkg, null, 2) + '\n')
-
-  var prompt = launchedFromCmd() ? '>' : '$'
-
-  console.log()
-  console.log('   change directory:')
-  console.log('     %s cd %s', prompt, dir)
-
-  console.log()
-  console.log('   install dependencies:')
-  console.log('     %s npm install', prompt)  
-
-  console.log()
-  console.log('   run server:')
-  console.log('     %s npm run server', prompt)
-  console.log()
-
-}
 
 /**
  * Create an app name from a directory path, fitting npm naming requirements.
@@ -275,25 +171,6 @@ function launchedFromCmd () {
     process.env._ === undefined
 }
 
-/**
- * Load template file.
- */
-
-function loadTemplate (name) {
-  var contents = fs.readFileSync(path.join(__dirname, '..', 'templates', (name + '.ejs')), 'utf-8')
-  var locals = Object.create(null)
-
-  function render () {
-    return ejs.render(contents, locals, {
-      escape: util.inspect
-    })
-  }
-
-  return {
-    locals: locals,
-    render: render
-  }
-}
 
 /**
  * Main program.
@@ -304,7 +181,7 @@ function main () {
   var destinationPath = program.args.shift() || '.'
 
   // App name
-  var appName = createAppName(path.resolve(destinationPath)) || 'hello-world'
+  var appName = createAppName(path.resolve(destinationPath)) || 'my-activeql'
 
   // Generate application
   emptyDirectory(destinationPath, function (empty) {
@@ -322,58 +199,4 @@ function main () {
       })
     }
   })
-}
-
-/**
- * Make the given dir relative to base.
- *
- * @param {string} base
- * @param {string} dir
- */
-
-function mkdir (base, dir) {
-  var loc = path.join(base, dir)
-
-  console.log('   \x1b[36mcreate\x1b[0m : ' + loc + path.sep)
-  mkdirp.sync(loc, MODE_0755)
-}
-
-/**
- * Generate a callback function for commander to warn about renamed option.
- *
- * @param {String} originalName
- * @param {String} newName
- */
-
-function renamedOption (originalName, newName) {
-  return function (val) {
-    warning(util.format("option `%s' has been renamed to `%s'", originalName, newName))
-    return val
-  }
-}
-
-/**
- * Display a warning similar to how errors are displayed by commander.
- *
- * @param {String} message
- */
-
-function warning (message) {
-  console.error()
-  message.split('\n').forEach(function (line) {
-    console.error('  warning: %s', line)
-  })
-  console.error()
-}
-
-/**
- * echo str > file.
- *
- * @param {String} file
- * @param {String} str
- */
-
-function write (file, str, mode) {
-  fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
-  console.log('   \x1b[36mcreate\x1b[0m : ' + file)
 }
